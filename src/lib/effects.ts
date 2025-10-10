f7a919e108c83437aaf9591208d6b44582f1aa3b
Christer Kaitila <breakdance@mcfunkypants.com>
2025-10-10 10:25:18 -0700

    card-splosion!!! (end game particle effect)

--- src/lib/effects.ts
+++ src/lib/effects.ts
@@ -9,6 +9,11 @@ export class Effects {
 
     parentScene!: Phaser.Scene;
     poofEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
+    cardEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
+    heartEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
+    clubEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
+    spadeEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
+    diamondEmitter!: Phaser.GameObjects.Particles.ParticleEmitter;
 
     constructor(currentScene: Phaser.Scene) {
         console.log("creating particle effect emitters!");
@@ -17,6 +22,28 @@ export class Effects {
             return;
         }
         this.parentScene = currentScene;
+        
+        // playing card particles for endgame celebration
+        let bounceRect = new Phaser.Geom.Rectangle(32,0,1200,900);
+        // FIXME: how do we access game rect?!?
+        // let bounceRect = currentScene.cameras.main.getBounds(); // ???
+        this.cardEmitter = currentScene.add.particles(0,0,ASSET_KEYS.CARDS, {
+            frame: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51],
+            lifespan: { min: 5000, max: 5000 }, // ms until it fades out
+            alpha: { start:1, end:0 }, // fade out
+            speed: { min: 200, max: 600 }, // pixels per second
+            angle: { min: 0, max: 360 }, // spin
+            gravityY: 2000, // fall
+            bounce: 0.7, 
+            collideLeft: true,
+            collideRight: true,
+            collideTop: false,
+            collideBottom: true, 
+            bounds: bounceRect,
+            frequency: -1, // wait to be triggered
+        });
+
+        // sparkles for placing cards cuccessfully
         this.poofEmitter = currentScene.add.particles(0,0,ASSET_KEYS.PARTICLE, {
             alpha: { start:1, end:0 }, // fade out
             lifespan: { min: 500, max: 1000 }, // ms until it fades out
@@ -31,7 +58,7 @@ export class Effects {
             // works, but positions are not relative to card
             // emitZone: [ { type: 'edge', source: new Phaser.Geom.Rectangle(0, 0, 100, 100), quantity: 250 }],
         });
-        
+
     } // end constructor
 
     // emit a burst of particles
@@ -60,10 +87,17 @@ export class Effects {
     winFX() {
         console.log("starting game win animation!");
         this.parentScene.sound.play(AUDIO_KEYS.GAME_WIN, { volume: 0.25 });
+
+        this.cardEmitter.explode(12,FOUNDATION_PILE_X_POSITIONS[0], FOUNDATION_PILE_Y_POSITION);
+        this.cardEmitter.explode(12,FOUNDATION_PILE_X_POSITIONS[1], FOUNDATION_PILE_Y_POSITION);
+        this.cardEmitter.explode(12,FOUNDATION_PILE_X_POSITIONS[2], FOUNDATION_PILE_Y_POSITION);
+        this.cardEmitter.explode(12,FOUNDATION_PILE_X_POSITIONS[3], FOUNDATION_PILE_Y_POSITION);
+
         this.poof(FOUNDATION_PILE_X_POSITIONS[0], FOUNDATION_PILE_Y_POSITION);
         this.poof(FOUNDATION_PILE_X_POSITIONS[1], FOUNDATION_PILE_Y_POSITION);
         this.poof(FOUNDATION_PILE_X_POSITIONS[2], FOUNDATION_PILE_Y_POSITION);
         this.poof(FOUNDATION_PILE_X_POSITIONS[3], FOUNDATION_PILE_Y_POSITION);
+
     }
 
 } // end Effects class
